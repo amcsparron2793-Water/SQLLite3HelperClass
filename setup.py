49b11419989f10f55e25728e6a5a17fd2854553c
Andrew McSparron <Amcsparron@albanyny.gov>
2023-09-08 07:58:33 -0400

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SQLLite3HelperClass',
+    version='1.0',
+    packages=['python_files'],
+    url='https://github.com/amcsparron2793-Water/SQLLite3HelperClass',
+    license='',
+    author='Amcsparron',
+    author_email='',
+    description='Initializes an sqlite3 database and has a basic query method. This class is meant to be subclassed and expanded.'
+)
