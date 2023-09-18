05b8086dfc7878bc3112b7668f79816ad911a27b
Andrew McSparron <Amcsparron@albanyny.gov>
2023-09-18 09:01:27 -0400

    updated batch script

--- building_script/sdist_and_twine.bat
+++ building_script/sdist_and_twine.bat
@@ -2,7 +2,7 @@
 
 cd C:\Users\amcsparron\Desktop\Python_Projects\SQLLite3HelperClass
 echo pwd changed to C:\Users\amcsparron\Desktop\Python_Projects\SQLLite3HelperClass
-
+REM need LICENSE.txt README.md setup.cfg setup.py - see https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
 REM DONT FORGET TO UPDATE setup.py, push commit to remote, and create a new release!! THEN run this!!!
 echo running sdist setup
 python setup.py sdist
